57977eb91fb97986085855539f51c4ac21c25a86
48268 - Marçorio Fortes <a48268@alunos.isel.pt>
2023-10-02 10:34:29 +0100

    Names changes

--- app/src/main/java/isel/gomuku/MainActivity.kt
+++ app/src/main/java/isel/gomuku/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -21,15 +20,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import isel.gomuku.gui.AUTHORSTEXTSIZE
-import isel.gomuku.gui.MENUBUTTONTEXTSIZE
-import isel.gomuku.gui.MENUBUTTONWIDTH
-import isel.gomuku.gui.MENUPADDING
+import isel.gomuku.gui.AUTHORS_TEXT_SIZE
+import isel.gomuku.gui.MENU_BUTTON_TEXT_SIZE
+import isel.gomuku.gui.MENU_BUTTON_WIDTH
+import isel.gomuku.gui.MENU_PADDING
 import isel.gomuku.logic.MenuState
 import isel.gomuku.ui.theme.GomukuTheme
 
@@ -52,15 +50,14 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun App(modifier: Modifier = Modifier) {
-    var menu by remember { mutableStateOf(MenuState.MAINMENU) }
+    var menu by remember { mutableStateOf(MenuState.MAIN_MENU) }
     when (menu) {
-        MenuState.MAINMENU -> MainMenu(Modifier.padding(MENUPADDING.dp).width(MENUBUTTONWIDTH.dp)) {
+        MenuState.MAIN_MENU -> MainMenu(Modifier.padding(MENU_PADDING.dp).width(MENU_BUTTON_WIDTH.dp)) {
             menu = it
         }
         MenuState.AUTHORS -> Biography {
             menu = it
         }
-
         MenuState.PLAY -> TODO()
     }
 }
@@ -68,14 +65,14 @@ fun App(modifier: Modifier = Modifier) {
 @Composable
 fun Biography(onClick: (MenuState) -> Unit) {
     Column {
-        Button(onClick = { onClick(MenuState.MAINMENU) }) {
+        Button(onClick = { onClick(MenuState.MAIN_MENU) }) {
             Text(text = "Main Menu")
         }
         Row(modifier = Modifier.fillMaxWidth(), Arrangement.SpaceEvenly) {
             Column(modifier = Modifier.fillMaxHeight(), Arrangement.SpaceEvenly) {
-                Text(text = "48323 Simão Cabral", fontSize = AUTHORSTEXTSIZE.sp)
-                Text(text = "49454 Eduardo Tavares", fontSize = AUTHORSTEXTSIZE.sp)
-                Text(text = "XXXXX Marçorio Fortes", fontSize = AUTHORSTEXTSIZE.sp)
+                Text(text = "48323 Simão Cabral", fontSize = AUTHORS_TEXT_SIZE.sp)
+                Text(text = "49454 Eduardo Tavares", fontSize = AUTHORS_TEXT_SIZE.sp)
+                Text(text = "48268 Marçorio Fortes", fontSize = AUTHORS_TEXT_SIZE.sp)
             }
         }
     }
@@ -86,13 +83,13 @@ fun MainMenu(modifier: Modifier, onClick: (MenuState) -> Unit) {
     Column(verticalArrangement = Arrangement.Center) {
 
         Button(onClick = { /*TODO*/ },modifier = modifier) {
-            Text(text = "Play", fontSize = MENUBUTTONTEXTSIZE.sp)
+            Text(text = "Play", fontSize = MENU_BUTTON_TEXT_SIZE.sp)
         }
         Button(onClick = { onClick(MenuState.AUTHORS) },modifier = modifier ) {
-            Text(text = "Authors", fontSize = MENUBUTTONTEXTSIZE.sp)
+            Text(text = "Authors", fontSize = MENU_BUTTON_TEXT_SIZE.sp)
         }
         Button(onClick = { /*TODO*/ }, modifier = modifier) {
-            Text(text = "Talk", fontSize = MENUBUTTONTEXTSIZE.sp)
+            Text(text = "Talk", fontSize = MENU_BUTTON_TEXT_SIZE.sp)
         }
     }
 }
