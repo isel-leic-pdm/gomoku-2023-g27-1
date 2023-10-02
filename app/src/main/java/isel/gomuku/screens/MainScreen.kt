e4cf6622c9722412d338f882ce7f704c4dea74a9
48323 - Simão Cabral <a48323@alunos.isel.pt>
2023-10-02 10:39:34 +0100

    Merge Refactoring

--- app/src/main/java/isel/gomuku/screens/MainScreen.kt
+++ app/src/main/java/isel/gomuku/screens/MainScreen.kt
@@ -7,7 +7,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.sp
-import isel.gomuku.helpers.MENUBUTTONTEXTSIZE
+
+import isel.gomuku.helpers.MENU_BUTTON_TEXT_SIZE
 import isel.gomuku.logic.MenuState
 
 @Composable
@@ -15,13 +16,13 @@ fun MainMenu(modifier: Modifier, onClick: (MenuState) -> Unit) {
     Column(verticalArrangement = Arrangement.Center) {
 
         Button(onClick = { /*TODO*/ },modifier = modifier) {
-            Text(text = "Play", fontSize = MENUBUTTONTEXTSIZE.sp)
+            Text(text = "Play", fontSize = MENU_BUTTON_TEXT_SIZE.sp)
         }
         Button(onClick = { onClick(MenuState.AUTHORS) },modifier = modifier ) {
-            Text(text = "Authors", fontSize = MENUBUTTONTEXTSIZE.sp)
+            Text(text = "Authors", fontSize = MENU_BUTTON_TEXT_SIZE.sp)
         }
         Button(onClick = { /*TODO*/ }, modifier = modifier) {
-            Text(text = "Talk", fontSize = MENUBUTTONTEXTSIZE.sp)
+            Text(text = "Talk", fontSize = MENU_BUTTON_TEXT_SIZE.sp)
         }
     }
 }
