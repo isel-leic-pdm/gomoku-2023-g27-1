3092993cb1e254496aeecd3f314622369bba8044
48323 - Simão Cabral <a48323@alunos.isel.pt>
2023-10-06 22:46:55 +0100

    GameLogic updated

--- app/src/main/java/isel/gomuku/gameLogic/Board.kt
+++ app/src/main/java/isel/gomuku/gameLogic/Board.kt
@@ -3,50 +3,28 @@ package isel.gomuku.gameLogic
 const val BOARD_SIZE = 15
 const val MAX_MOVES = BOARD_SIZE * BOARD_SIZE
 const val VICTORY_LINE = 5
-const val EXACTLY_VICTORY_LINE = true
 
-/*
-    companion object {
-        fun deserialize(input: String): isel.gomuku.GameLogic.Board {
-            val lines = input.split("\n")
-            val kind = lines[0]
-            val moves = lines.drop(1).filter { it.isNotEmpty() }.map { isel.gomuku.gameLogic.Move.deserialize(it) }
-            val lastPlayer = if(moves.isEmpty()) isel.gomuku.gameLogic.Player.WHITE else moves.last().player
-            return when (kind) {
-                isel.gomuku.GameLogic.BoardRun::class.simpleName -> isel.gomuku.GameLogic.BoardRun(moves, lastPlayer)
-                isel.gomuku.GameLogic.BoardDraw::class.simpleName -> isel.gomuku.GameLogic.BoardDraw(moves)
-                isel.gomuku.GameLogic.BoardWinner::class.simpleName -> isel.gomuku.GameLogic.BoardWinner(moves, lastPlayer)
-                else -> { throw IllegalArgumentException("There is no board type for input $kind")}
-            }
-        }
-    }
-    fun serialize() = "${this::class.simpleName}\n${moves.joinToString("\n") { it.serialize() }}"
- */
-
-sealed class Board(val moves: List<Move>) {
-    abstract fun play(pos: Position, player: Player): Board
-
-    fun get(pos: Position): Move? {
-        return moves.find { it.pos == pos }
-    }
+interface Board {
+    fun play(pos: Position, player: Player): Board
 }
 
-class BoardDraw(moves: List<Move>) : Board(moves) {
+class BoardDraw(val moves: List<Move>) : Board {
+
     override fun play(pos: Position, player: Player): Board {
         throw IllegalStateException("This game has already finished with a draw.")
     }
 }
 
-class BoardWinner(moves: List<Move>, val winner: Player) : Board(moves) {
+class BoardWinner(val moves: List<Move>, val winner: Player) : Board {
     override fun play(pos: Position, player: Player): Board {
         throw IllegalStateException("The player $winner won this game.")
     }
 }
 
-class BoardRun(moves: List<Move> = emptyList(), val lastPlayer: Player = Player.WHITE) : Board(moves) {
+class BoardRun(val moves: List<Move> = emptyList(), val lastPlayer: Player = Player.WHITE) : Board {
     override fun play(pos: Position, player: Player): Board {
-        require(lastPlayer != player) { "isel.gomuku.gameLogic.Player $player cannot play twice!" }
-        require(!moves.any { m -> m.pos == pos }) { "isel.gomuku.gameLogic.Position $pos occupied! Please play on an empty position." }
+        require(lastPlayer != player) { "Player $player cannot play twice!" }
+        require(!moves.any { m -> m.pos == pos }) { "Position $pos occupied! Please play on an empty position." }
         val m = Move(player, pos)
         return when {
             checkWinner(m, pos) -> BoardWinner(moves + m, player)
@@ -63,13 +41,12 @@ class BoardRun(moves: List<Move> = emptyList(), val lastPlayer: Player = Player.
                 ||checkWinCondition(countPieces(plMoves, pos, Pair(-1, 1)) + countPieces(plMoves, pos, Pair(1, -1)) + 1)
     }
 
-    private fun checkWinCondition(int: Int): Boolean{
-        return if (EXACTLY_VICTORY_LINE) int == VICTORY_LINE
-        else int >= VICTORY_LINE
-    }
+    private fun checkWinCondition(int: Int): Boolean =
+       int >= VICTORY_LINE
 
-    private fun countPieces(moves: List<Move>, lastPos: Position, direction: Pair<Int,Int>, counter: Int = 0): Int{
-        val checkPos : Position = (lastPos + direction) ?: return counter
+
+    private fun countPieces(moves: List<Move>, lastPlay: Position, direction: Pair<Int,Int>, counter: Int = 0): Int{
+        val checkPos : Position = (lastPlay + direction) ?: return counter
         val move = moves.find { move -> move.pos == checkPos } ?: return counter
         return countPieces(moves, move.pos, direction, counter + 1)
     }
