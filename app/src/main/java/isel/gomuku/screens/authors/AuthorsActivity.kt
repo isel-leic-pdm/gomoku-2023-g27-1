6a4cb8f151db585e30e23d06ca99e67ee66902a2
eduardoisel <eduardodinis3@gmail.com>
2023-12-28 11:38:46 +0000

    Added email sending functionality (With school emails)

--- app/src/main/java/isel/gomuku/screens/authors/AuthorsActivity.kt
+++ app/src/main/java/isel/gomuku/screens/authors/AuthorsActivity.kt
@@ -1,18 +1,22 @@
 package isel.gomuku.screens.authors
 
+import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import isel.gomuku.R
 import isel.gomuku.ui.theme.GomukuTheme
 
-class AuthorsScreenActivity: ComponentActivity(){
+class AuthorsScreenActivity : ComponentActivity() {
 
-    companion object{
+    companion object {
         fun navigate(source: ComponentActivity) {
             val intent = Intent(source, AuthorsScreenActivity::class.java)
             source.startActivity(intent)
@@ -29,10 +33,42 @@ class AuthorsScreenActivity: ComponentActivity(){
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    AuthorsScreen{this.finish()}
+                    AuthorsScreen(
+                        backHandle = { this.finish() },
+                        sendEmail = { sendEmailIntent(it, "subject", "body") }
+                    )
                 }
             }
         }
     }
 
+    //make content and subject hardcoded?
+    fun sendEmailIntent(email: String, subject: String, content: String) {
+        try {
+            val intent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:")
+                putExtra(
+                    Intent.EXTRA_EMAIL,
+                    arrayOf(email)
+                )
+                putExtra(
+                    Intent.EXTRA_SUBJECT,
+                    subject
+                )
+                putExtra(Intent.EXTRA_TEXT, content)
+            }
+
+            //nao experimentado. Talvez envie email automaticamente?
+            //startActivity(Intent.createChooser(intent, "sending"))
+            startActivity(intent)
+        } catch (e: ActivityNotFoundException) {
+            Toast
+                .makeText(
+                    this,
+                    R.string.activity_info_no_suitable_app,
+                    Toast.LENGTH_LONG
+                )
+                .show()
+        }
+    }
 }
--- app/src/main/java/isel/gomuku/screens/authors/DrawAuthors.kt
+++ app/src/main/java/isel/gomuku/screens/authors/DrawAuthors.kt
@@ -1,11 +1,12 @@
 package isel.gomuku.screens.authors
 
-import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -16,20 +17,32 @@ import isel.gomuku.utils.AUTHORS_TEXT_SIZE
 import isel.gomuku.screens.component.NavigationHandlers
 import isel.gomuku.screens.component.TopBar
 
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AuthorsScreen(backHandle: () -> Unit) {
-    Scaffold(topBar = { TopBar(navigationHandlers = NavigationHandlers(onBackHandler = backHandle)) }){}
+fun AuthorsScreen(backHandle: () -> Unit, sendEmail: (String) -> Unit) {
+    Scaffold(topBar = { TopBar(navigationHandlers = NavigationHandlers(onBackHandler = backHandle)) }) { pad ->
+        Column(Modifier.padding(vertical = pad.calculateTopPadding())) {
 
-    Column {
-
-        Row(modifier = Modifier.fillMaxWidth(), Arrangement.SpaceEvenly) {
-            Column(modifier = Modifier.fillMaxHeight(), Arrangement.SpaceEvenly) {
-                Text(text = "48323 Simão Cabral", fontSize = AUTHORS_TEXT_SIZE.sp)
-                Text(text = "49454 Eduardo Tavares", fontSize = AUTHORS_TEXT_SIZE.sp)
-                Text(text = "48268 Marçorio Fortes", fontSize = AUTHORS_TEXT_SIZE.sp)
+            Row(modifier = Modifier.fillMaxWidth(), Arrangement.SpaceEvenly) {
+                Column(modifier = Modifier.fillMaxHeight(), Arrangement.SpaceEvenly) {
+                    Text(text = "48323 Simão Cabral", fontSize = AUTHORS_TEXT_SIZE.sp)
+                    Text(text = "49454 Eduardo Tavares", fontSize = AUTHORS_TEXT_SIZE.sp)
+                    Text(text = "48268 Marçorio Fortes", fontSize = AUTHORS_TEXT_SIZE.sp)
+                    SendEmail(sendEmail = sendEmail, modifier = Modifier)
+                }
             }
+
         }
+
+    }
+
+
+}
+
+@Composable
+fun SendEmail(sendEmail: (String) -> Unit, modifier: Modifier) {
+    Button(onClick = { sendEmail("A49454@alunos.isel.pt,A48268@alunos.isel.pt,A48323@alunos.isel.pt") }, modifier) {
+        Text(text = "Send Email", fontSize = AUTHORS_TEXT_SIZE.sp)
     }
 }
