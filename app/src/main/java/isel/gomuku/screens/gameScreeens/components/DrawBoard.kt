aee64158c8260fce314cf7482fe4f2e800abb663
eduardoisel <eduardodinis3@gmail.com>
2023-12-28 11:39:53 +0000

    Conditional param to display your color on remote games

--- app/src/main/java/isel/gomuku/screens/gameScreeens/components/DrawBoard.kt
+++ app/src/main/java/isel/gomuku/screens/gameScreeens/components/DrawBoard.kt
@@ -12,15 +12,16 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import isel.gomuku.services.local.gameLogic.Player
@@ -31,18 +32,32 @@ private const val ROW_PADDING = 5
 private const val CELL_PADDING = 0.5
 private const val BORDER_WIDTH = 1
 private const val CIRCLE_ALIGN = 4
+
+
+const val bottomRightCorner = "extremity"
+const val ignore = "ignore"
+
 @Composable
 fun DrawBoard(
     modifier: Modifier,
     boardSize: Int,
     makePlay: (Position) -> Unit,
-    moves: MutableMap<Position, Player?>
+    moves: MutableMap<Position, Player?>,
+    player: Player? = null
 ) {
-    val cellSize = (LocalConfiguration.current.screenWidthDp / boardSize) -1
+    val cellSize = (LocalConfiguration.current.screenWidthDp / boardSize) - 1
     val backGroundColor = Color(red = 246, green = 206, blue = 5)
-    Column(modifier = modifier,horizontalAlignment = Alignment.CenterHorizontally) {
-        Box(modifier = Modifier.background(backGroundColor).border(BORDER_WIDTH.dp, Color.Black)) {
+    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+        //Might be necessary for remote game
+        if (player != null)
+            Text(text = "You are playing as: $player")
+        Box(
+            modifier = Modifier
+                .background(backGroundColor)
+                .border(BORDER_WIDTH.dp, Color.Black)
+        ) {
             Row(modifier = Modifier.padding(ROW_PADDING.dp)) {
+
                 repeat(boardSize) { column ->
                     Column {
                         repeat(boardSize) { row ->
@@ -53,6 +68,7 @@ fun DrawBoard(
                                 .border(BORDER_WIDTH.dp, color = Color.Gray)
                                 .wrapContentSize(Alignment.Center)
                                 .clickable { makePlay(pos) }
+                                .testTag(positionTag(boardSize, pos))
                             ) {
                                 val color = when (moves[pos]) {
                                     Player.BLACK -> Color.Black
@@ -76,17 +92,20 @@ fun DrawBoard(
 }
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-@OptIn(ExperimentalMaterial3Api::class)
 @Preview(showBackground = true)
 @Composable
 fun Preview() {
     GomukuTheme {
         val grid = 15
-        val size = (LocalConfiguration.current.screenWidthDp / grid) -1
+        val size = (LocalConfiguration.current.screenWidthDp / grid) - 1
         Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
             val backGroundColor = Color(red = 246, green = 206, blue = 5)
             Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                Box(modifier = Modifier.background(backGroundColor).border(BORDER_WIDTH.dp, Color.Black)) {
+                Box(
+                    modifier = Modifier
+                        .background(backGroundColor)
+                        .border(BORDER_WIDTH.dp, Color.Black)
+                ) {
                     Row(modifier = Modifier.padding(ROW_PADDING.dp)) {
                         repeat(grid) { column ->
                             Column() {
@@ -96,12 +115,12 @@ fun Preview() {
                                         .padding(0.5.dp)
                                         .border(BORDER_WIDTH.dp, color = Color.Gray)
                                         .wrapContentSize(Alignment.Center)
-                                        .clickable {  }
+                                        .clickable { }
                                     ) {
                                         val color = Color.Black
                                         Box(
                                             modifier = Modifier
-                                                .size((size-4).dp)
+                                                .size((size - 4).dp)
                                                 .clip(CircleShape)
                                                 .background(color)
                                         )
@@ -116,3 +135,11 @@ fun Preview() {
         }
     }
 }
+
+
+//For testing
+//if result is modified change also LocalGameTest
+private fun positionTag(gridSize: Int, pos: Position) =
+    if (pos.lin == gridSize - 1 && pos.col == gridSize - 1)
+        bottomRightCorner
+    else ignore
